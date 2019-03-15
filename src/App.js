316a77e444e2bd631a1f581d0c1a97dfa1948a2e
Krauchman <rauan.omarov@nu.edu.kz>
2019-03-16 01:46:09 +0600

    Modified book add form

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import React, { Component } from 'react';
-import Routes from '../src/components/Routes';
-import TopNavigation from './components/topNavigation';
-import SideNavigation from './components/sideNavigation';
-import Footer from './components/Footer';
-import './index.css';
+import React, { Component } from "react";
+import Routes from "../src/components/Routes";
+import TopNavigation from "./components/topNavigation";
+import SideNavigation from "./components/sideNavigation";
+import Footer from "./components/Footer";
+import "./index.css";
 
 class App extends Component {
-  
   render() {
     return (
-        <div className="flexible-content">
-          <TopNavigation />
-          <SideNavigation />
-          <main id="content" className="p-5">
-            <Routes />
-          </main>
-          <Footer />
-        </div>
+      <div className="flexible-content">
+        <TopNavigation />
+        <SideNavigation />
+        <main id="content" className="p-5">
+          <Routes />
+        </main>
+        <Footer />
+      </div>
     );
   }
 }

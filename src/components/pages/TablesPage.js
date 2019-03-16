1579585f52231826a07d1418ce39fc873e164595
Krauchman <rauan.omarov@nu.edu.kz>
2019-03-16 15:42:43 +0600

    Modified book list. Added book details modal

--- src/components/pages/TablesPage.js
+++ src/components/pages/TablesPage.js
@@ -12,68 +12,68 @@ import {
 import BookList from "./sections/BookList";
 import BookAddForm from "./sections/BookAdd";
 
-const TablesPage = () => {
-  return (
-    <>
-      <MDBRow>
-        <MDBCol md="12">
-          <MDBCard className="mt-5">
-            <MDBView className="gradient-card-header blue darken-2">
-              <h4 className="h4-responsive text-white">Basic tables</h4>
-            </MDBView>
-            <MDBCardBody>
-              <BookAddForm />
-              <h3 className="mt-5 text-left">
-                <strong>Currently you have these books in your account:</strong>
-              </h3>
-              <p />
-              <MDBTable striped>
-                <MDBTableHead>
-                  <tr>
-                    <th>#</th>
-                    <th>Title</th>
-                    <th>Author</th>
-                    <th>Description</th>
-                  </tr>
-                </MDBTableHead>
-                <BookList userType="currentHolder" />
-              </MDBTable>
-              <h3 className="mt-5 text-left">
-                <strong>Books owned by you:</strong>
-              </h3>
-              <p />
-              <MDBTable striped>
-                <MDBTableHead>
-                  <tr>
-                    <th>#</th>
-                    <th>Title</th>
-                    <th>Author</th>
-                    <th>Description</th>
-                  </tr>
-                </MDBTableHead>
-                <BookList userType="owner" />
-              </MDBTable>
-              <h3 className="mt-5 text-left">
-                <strong>Marked books:</strong>
-              </h3>
-              <p />
-              <MDBTable>
-                <MDBTableHead color="primary-color" textWhite>
-                  <tr>
-                    <th>#</th>
-                    <th>Title</th>
-                    <th>Author</th>
-                    <th>Description</th>
-                  </tr>
-                </MDBTableHead>
-                <BookList markedBooks />
-              </MDBTable>
-            </MDBCardBody>
-          </MDBCard>
-        </MDBCol>
-      </MDBRow>
-    </>
-  );
-};
+class TablesPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      modalBook: false
+    };
+  }
+
+  toggleBook = () => {
+    this.setState({
+      modalBook: !this.state.modalBook
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <MDBRow>
+          <MDBCol md="12">
+            <MDBCard className="mt-5">
+              <MDBView className="gradient-card-header blue darken-2">
+                <h4 className="h4-responsive text-white">Basic tables</h4>
+              </MDBView>
+              <MDBCardBody>
+                <BookAddForm />
+                <h3 className="mt-5 text-left">
+                  <strong>
+                    Currently you have these books in your account:
+                  </strong>
+                </h3>
+                <p />
+                <BookList
+                  userType="currentHolder"
+                  modalBook={this.state.modalBook}
+                  toggleBook={this.toggleBook}
+                />
+                <h3 className="mt-5 text-left">
+                  <strong>Books owned by you:</strong>
+                </h3>
+                <p />
+                <BookList
+                  userType="owner"
+                  modalBook={this.state.modalBook}
+                  toggleBook={this.toggleBook}
+                />
+                <h3 className="mt-5 text-left">
+                  <strong>Marked books:</strong>
+                </h3>
+                <p />
+                <BookList
+                  markedBooks
+                  modalBook={this.state.modalBook}
+                  toggleBook={this.toggleBook}
+                />
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+        </MDBRow>
+      </>
+    );
+  }
+}
 
 export default TablesPage;
--- src/components/pages/sections/BookList.js
+++ src/components/pages/sections/BookList.js
@@ -1,31 +1,57 @@
 import React from "react";
 import firebase from "../../../Firestore";
-import { MDBTableBody } from "mdbreact";
+import {
+  MDBTableBody,
+  MDBCard,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardText,
+  MDBCol,
+  MDBRow,
+  MDBContainer,
+  Input,
+  MDBModal,
+  MDBModalHeader,
+  MDBModalBody
+} from "mdbreact";
 
-export class BookItem extends React.Component {
+export class BookCard extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      title: props.book.title,
-      description: props.book.description,
-      author: props.book.author,
-      likes: props.book.usersLiked.length,
-      comments: 0, // TODO
-      owner: props.book.owner,
-      currentHolder: props.book.currentHolder,
-      uid: props.book.uid
+      book: this.props.book
     };
   }
 
+  shortText = text => {
+    const limit = this.props.charLimit || 170;
+    if (text.length > limit) return text.substring(0, limit) + "...";
+    return text.substring(0, text.length);
+  };
+
+  toggleBook = () => {
+    this.props.toggleBook(this.state.book);
+  };
+
   render() {
     return (
-      <tr>
-        <td>{this.props.index}</td>
-        <td>{this.state.title}</td>
-        <td>{this.state.author}</td>
-        <td>{this.state.description}</td>
-      </tr>
+      <MDBCol>
+        <MDBCard
+          onClick={this.toggleBook}
+          style={{ height: "20rem", width: "12rem" }}
+        >
+          <MDBCardBody>
+            <h5 className="font-weight-bold mb-3">{this.state.book.title}</h5>
+            <p className="font-weight-bold grey-text">
+              by {this.state.book.author}
+            </p>
+            <MDBCardText>
+              {this.shortText(this.state.book.description)}
+            </MDBCardText>
+          </MDBCardBody>
+        </MDBCard>
+      </MDBCol>
     );
   }
 }
@@ -35,7 +61,8 @@ class BookList extends React.Component {
     super(props);
     this.state = {
       user: null,
-      books: []
+      books: [],
+      bookOpened: null
     };
   }
 
@@ -128,32 +155,105 @@ class BookList extends React.Component {
       }
     });
   }
+
+  split = (arr, len) => {
+    let res = [];
+    for (let i = 0; i < arr.length; i += len) {
+      res.push(arr.slice(i, Math.min(i + len, arr.length)));
+    }
+    return res;
+  };
+
+  toggleBook = (book = null) => {
+    this.setState({
+      bookOpened: book
+    });
+    this.props.toggleBook();
+  };
+
   render() {
     const { user } = this.state;
     const { books } = this.state;
+    const splitBooks = this.split(books, 4);
 
-    // console.log(user);
+    const bookCardsTable = splitBooks.map(block => {
+      return (
+        <div key={block[0].uid}>
+          <MDBRow>
+            {block.map(book => {
+              return (
+                <BookCard
+                  toggleBook={this.toggleBook}
+                  book={book}
+                  key={book.uid}
+                />
+              );
+            })}
+          </MDBRow>
+          <br />
+        </div>
+      );
+    });
+
     return (
       <React.Fragment>
-        <MDBTableBody>
-          {books.map((book, index) => {
-            return <BookItem book={book} index={index + 1} key={book.uid} />;
-          })}
-        </MDBTableBody>
-        {user && books.length === 0 && (
-          <MDBTableBody>
-            <tr>
-              <td>You have no books.</td>
-            </tr>
-          </MDBTableBody>
-        )}
-        {!user && (
-          <MDBTableBody>
-            <tr>
-              <td>Please, log in.</td>
-            </tr>
-          </MDBTableBody>
-        )}
+        <MDBContainer>
+          {bookCardsTable}
+          {books.length === 0 && user && <p>You have no books.</p>}
+          {!user && <p>Please, log in.</p>}
+        </MDBContainer>
+        <MDBModal
+          size="lg"
+          isOpen={this.props.modalBook && this.state.bookOpened != null}
+          toggle={this.toggleBook}
+        >
+          <MDBModalHeader toggle={this.toggleBook}>Book Details</MDBModalHeader>
+          <MDBModalBody>
+            <MDBContainer>
+              <MDBRow>
+                <MDBCol size="2" className="grey-text">
+                  Title:
+                </MDBCol>
+                <MDBCol>
+                  {this.state.bookOpened != null && this.state.bookOpened.title}
+                </MDBCol>
+              </MDBRow>
+              <br />
+
+              <MDBRow>
+                <MDBCol size="2" className="grey-text">
+                  Author:
+                </MDBCol>
+                <MDBCol>
+                  {this.state.bookOpened != null &&
+                    this.state.bookOpened.author}
+                </MDBCol>
+              </MDBRow>
+              <br />
+
+              <MDBRow>
+                <MDBCol size="2" className="grey-text">
+                  Publisher:
+                </MDBCol>
+                <MDBCol>
+                  {this.state.bookOpened != null &&
+                    this.state.bookOpened.publisher}
+                </MDBCol>
+              </MDBRow>
+              <br />
+
+              <MDBRow>
+                <MDBCol size="2" className="grey-text">
+                  Description:
+                </MDBCol>
+                <MDBCol>
+                  {this.state.bookOpened != null &&
+                    this.state.bookOpened.description}
+                </MDBCol>
+              </MDBRow>
+            </MDBContainer>
+          </MDBModalBody>
+        </MDBModal>
       </React.Fragment>
     );
   }
